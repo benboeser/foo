e6baa3346c551a5fe6fb221cbbbc364bb4d852f5
David Farr <dfarr219@gmail.com>
2014-09-02 10:29:45 -0700

    Create server.js

---
+++ server/src/server.js
@@ -0,0 +1,224 @@
+var async = require('async');
+var colors = require('colors');
+var express = require('express');
+var glob = require('glob');
+var merge = require('merge');
+var passport = require('passport');
+var path = require('path');
+var sass = require('node-sass');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Load configuration
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+global.config = require('./../config');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Express application
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+var app = express();
+var api = {};
+var webhooks = {};
+
+app.use(require('body-parser').json());
+app.use(require('cookie-parser')());
+app.use(require('cookie-session')({
+    secret: config.server.security.sessionSecret,
+    cookie: {
+        maxAge: config.server.security.cookieMaxAge
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// custom middleware
+app.use('/api', require('./middleware/param'));
+app.use('/github/webhook', require('./middleware/param'));
+app.use('/api', require('./middleware/authenticated'));
+
+async.series([
+
+    function(callback) {
+        console.log('checking configs'.bold);
+
+        if(config.server.http.protocol !== 'http' && config.server.http.protocol !== 'https') {
+            throw new Error('PROTOCOL must be "http" or "https"');
+        }
+
+        if(config.server.github.protocol !== 'http' && config.server.github.protocol !== 'https') {
+            throw new Error('GITHUB_PROTOCOL must be "http" or "https"');
+        }
+
+        console.log('✓ '.bold.green + 'configs seem ok');
+
+        var url = require('./services/url');
+
+        console.log('Host:        ' + url.baseUrl);
+        console.log('GitHub:      ' + url.githubBase);
+        console.log('GitHub-Api:  ' + url.githubApiBase);
+        callback();
+    },
+
+    function(callback) {
+
+        console.log('bootstrap static files'.bold);
+
+        config.server.static.forEach(function(p) {
+            app.use(sass.middleware({
+                src: p,
+                dest: p,
+                outputStyle: 'compressed',
+                force: config.server.always_recompile_sass
+            }));
+            app.use(express.static(p));
+        });
+        callback();
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Bootstrap mongoose
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function(callback) {
+
+        console.log('bootstrap mongoose'.bold);
+
+        var mongoose = require('mongoose');
+
+        mongoose.connect(config.server.mongodb.uri, {
+            server: {
+                socketOptions: {
+                    keepAlive: 1
+                }
+            }
+        });
+
+        global.models = {};
+
+        async.eachSeries(config.server.documents, function(p, callback) {
+            glob(p, function(err, file) {
+                if (file && file.length) {
+                    file.forEach(function(f) {
+                        try {
+                            global.models = merge(global.models, require(f));
+                            console.log('✓ '.bold.green + path.relative(process.cwd(), f));
+                        } catch (ex) {
+                            console.log('✖ '.bold.red + path.relative(process.cwd(), f));
+                            console.log(ex.stack);
+                        }
+                    });
+                    callback();
+                }
+            });
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Bootstrap passport
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function(callback) {
+
+        console.log('bootstrap passport'.bold);
+
+        async.eachSeries(config.server.passport, function(p, callback) {
+            glob(p, function(err, file) {
+                if (file && file.length) {
+                    file.forEach(function(f) {
+                        console.log('✓ '.bold.green + path.relative(process.cwd(), f));
+                        require(f);
+                    });
+                }
+                callback();
+            });
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Bootstrap controller
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function(callback) {
+
+        console.log('bootstrap controller'.bold);
+
+        async.eachSeries(config.server.controller, function(p, callback) {
+            glob(p, function(err, file) {
+                if (file && file.length) {
+                    file.forEach(function(f) {
+                        try {
+                            app.use('/', require(f));
+                            console.log('✓ '.bold.green + path.relative(process.cwd(), f));
+                        } catch (ex) {
+                            console.log('✖ '.bold.red + path.relative(process.cwd(), f));
+                            console.log(ex.stack);
+                        }
+                    });
+                }
+                callback();
+            });
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Bootstrap api
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function(callback) {
+
+        console.log('bootstrap api'.bold);
+
+        async.eachSeries(config.server.api, function(p, callback) {
+            glob(p, function(err, file) {
+                if (file && file.length) {
+                    file.forEach(function(f) {
+                        console.log('✓ '.bold.green + path.relative(process.cwd(), f));
+                        api[path.basename(f, '.js')] = require(f);
+                    });
+                }
+                callback();
+            });
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Bootstrap webhooks
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function(callback) {
+
+        console.log('bootstrap webhooks'.bold);
+
+        async.eachSeries(config.server.webhooks, function(p, callback) {
+            glob(p, function(err, file) {
+                if (file && file.length) {
+                    file.forEach(function(f) {
+                        console.log('✓ '.bold.green + path.relative(process.cwd(), f));
+                        webhooks[path.basename(f, '.js')] = require(f);
+                    });
+                }
+                callback();
+            });
+        }, callback);
+    }
+
+], function(err, res) {
+    console.log('\n✓ '.bold.green + 'bootstrapped, '.bold + 'app listening on localhost:' + config.server.localport);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Handle api calls
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.all('/api/:obj/:fun', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    api[req.params.obj][req.params.fun](req, function(err, obj) {
+        if(err) {
+            return res.send(err.code > 0 ? err.code : 500, JSON.stringify(err.text || err));
+        }
+        obj ? res.send(JSON.stringify(obj)) : res.send();
+    });
+});
+
+module.exports = app;
